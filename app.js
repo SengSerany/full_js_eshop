8a567189d34d5a847dd8a4c8fe3a716cc113ccc3
SengSerany <cranyseng@gmail.com>
2020-01-09 22:44:09 +0100

    routes show_item ok

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('./models/Items');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/eshop', {useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1/eshop', {useNewUrlParser: true});
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -13,5 +13,6 @@ db.once('open', function() {
 
 app.set('view engine', 'ejs');
 app.use('/', require('./controllers/items'));
+app.use('/items', require('./controllers/items'));
 
 app.listen(3000);
--- controllers/items.js
+++ controllers/items.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 let Item = require('./../models/Items');
 
+//index
 router.get('/', (req, res) => {
     Item.find({}).then( items => {
         res.render('items/index', {items: items});
     })
-})
+});
+
+//new
+
+//create
+
+//edit
+
+//update
+
+//delete
+
+//show
+router.get('/items/:id', (req, res) => {
+    Item.findById(req.params.id).then(item => {
+        res.render('items/show', {item: item});
+    })
+});
+
 
 module.exports = router;
